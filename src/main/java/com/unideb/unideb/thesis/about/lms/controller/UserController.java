788771fcd539eb878a2d9a7e029c7ba8f854465d
phoopwintlatt <phoopwintlatt@gmail.com>
2023-11-02 15:10:51 +0100

    updated

--- src/main/java/com/unideb/unideb/thesis/about/lms/controller/UserController.java
+++ src/main/java/com/unideb/unideb/thesis/about/lms/controller/UserController.java
@@ -3,11 +3,12 @@ package com.unideb.unideb.thesis.about.lms.controller;
 import com.unideb.unideb.thesis.about.lms.model.User;
 import com.unideb.unideb.thesis.about.lms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -16,5 +17,10 @@ public class UserController {
     @PostMapping("/user") //posting the data on the given path
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
